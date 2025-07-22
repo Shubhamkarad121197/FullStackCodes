d2c28f5d08d3bb81b89cfb25cdb8395db465d124
shubham karad <shubham.karad12@gmail.com>
2025-07-22 23:34:05 +0530

    array method

---
+++ JS-101/session4/arrayMethods.js
@@ -0,0 +1,24 @@
+// Write a JavaScript program that demonstrates the use of the following array methods: push(), shift(), unshift(), and pop().
+// Step 1: Create an empty array
+// Step 2: Use push()
+// Add three elements to the end of the array using the push() method
+// Print the array after each push operation
+// Step 3: Use shift()
+// Remove the first element of the array using the shift() method
+// Print the array after the shift operation
+// Step 4: Use unshift()
+// Add two new elements to the beginning of the array using the unshift() method
+// Print the array after each unshift operation
+// Step 5: Use pop()
+// Remove the last element of the array using the pop() method
+// Print the array after the pop operation
+
+let arr=[];
+arr.push('I','Am','Shubham');
+console.log(arr);
+arr.shift();
+console.log(arr);
+arr.unshift('I',"Have");
+console.log(arr);
+arr.pop();
+console.log(arr);
