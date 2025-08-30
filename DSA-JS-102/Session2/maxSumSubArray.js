bddc5f1cfe67eb72d04e6540e8812a10d80aff3e
shubham karad <shubham.karad12@gmail.com>
2025-08-30 17:57:17 +0530

    maxSumSubArray

---
+++ DSA-JS-102/Session2/maxSumSubArray.js
@@ -0,0 +1,45 @@
+// Problem Description
+// Given an array, find a non-empty contiguous subarray with the largest sum.
+
+// Input format
+// There are 2 lines of input.
+
+// First line contains N, the number of elements in the array.
+
+// Second line contains N space separated numbers, representing the array elements.
+
+// Output format
+// A single integer which is the maximum possible contiguous subarray sum
+
+// Constraints
+// 1 ≤ N ≤ 10^5
+
+// -10^9 ≤ A[i] ≤ 10^9
+
+// Sample Input 1
+// 8
+
+// -2 -3 4 -1 -2 1 5 -3
+
+// Sample Output 1
+// 7
+
+// Explanation 1
+// The subarray [4 -1 -2 1 5] has sum 7, which is the maximum possible.
+let arr=[-2, -3, 4, -1, -2, 1, 5, -3];
+function maxSumSubArray(arr){
+    let prefixSum=[];
+    prefixSum[0]=arr[0];
+    for(let i=1;i<arr.length;i++){
+        prefixSum[i]=prefixSum[i-1]+arr[i]
+    }
+    let maxSum=prefixSum[0]
+    let minPrefix=0;
+
+    for(let i=0;i<arr.length;i++){
+        maxSum=Math.max(maxSum,prefixSum[i]-minPrefix)
+        minPrefix=Math.min(minPrefix,prefixSum[i])
+    }
+    return maxSum;
+}
+console.log(maxSumSubArray(arr))
